5a1b4c5b8f028e16e7c9cb5ffabbbff82d642185
Isaac Pierce <contact@isaacdpierce.com>
2018-09-07 21:50:24 -0700

    Finish likeLyric logic

--- client/components/SongDetail.js
+++ client/components/SongDetail.js
@@ -2,10 +2,10 @@ import React, { Component } from 'react';
 import { graphql } from 'react-apollo';
 import { Link } from 'react-router';
 import fetchSong from '../queries/fetchSong'
-import LyricCreate from './LyricCreate'
+import LyricCreate from './LyricCreate';
+import LyricList from './LyricList'
 
 class SongDetail extends Component {
-
   
   render() {
     const { song } = this.props.data;
@@ -16,6 +16,7 @@ class SongDetail extends Component {
       <div>
         <Link to="/" >Back</Link>
         <h3>{song.title}</h3>
+        <LyricList lyrics={song.lyrics} />
         <LyricCreate songId={this.props.params.id} />
       </div>
     )
